0cadf9f905d8788dc0999d4addd506d03949d33c
rkepha <kephaokari@gmail.com>
2017-12-16 11:58:45 +0300

    created the review

--- app/auth/view.py
+++ app/auth/view.py
@@ -1,5 +1,3 @@
-from flask import render_template, request, redirect, url_for
-from . import auth
 from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from flask_login import login_user,logout_user,login_required
